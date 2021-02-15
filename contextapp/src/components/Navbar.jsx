18cd8211276d5412dd42d2215fc3d3bca66c1b3a
Raquel Dionísio <dionisio.raquel@gmail.com>
2021-02-15 18:37:46 +0100

    commenting the code

--- contextapp/src/components/Navbar.jsx
+++ contextapp/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { ThemeContext } from "../contexts/ThemeContext";
 class Navbar extends React.Component {
   render() {
     return (
+      //another way of accessing Context | this one allows you to use more than one context in the same component and also works in functional components
       <ThemeContext.Consumer>
         {(context) => {
           const { isLightTheme, light, dark } = context;
